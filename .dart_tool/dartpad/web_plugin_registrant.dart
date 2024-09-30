e9126951bccd78595bbd3820d1f646cc293cbee3
jonathancaleb <jonathancaleb230@gmail.com>
2024-09-30 15:12:45 +0300

    updated "packages :package:"

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,11 +6,25 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:flutter_charset_detector_web/flutter_charset_detector_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FilePickerWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  CharsetDetectorWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
