f88a19be1615611ad2b046ed092f9788a5372cdc
jonathancaleb <jonathancaleb230@gmail.com>
2024-10-10 11:01:35 +0300

    :package: upgraded core packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cashorbit/firebase_options.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
